48f14d6ea2604f870c6c753b191d58ed5c84fbe2
Pavel <qqq@io.com>
2015-04-16 17:24:48 +0500

    converter

--- src/main/java/com/microtest/Converter/HelloWorld.java
+++ src/main/java/com/microtest/Converter/HelloWorld.java
@@ -1,10 +1,26 @@
-package com.microtest.Converter;
-import static spark.Spark.*;
+package com.microtest.converter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) throws IOException
 
+    {
         System.out.println("Hello World");
+
+        System.out.println("yjykhkjhkjhk".toUpperCase());
+
+        char c ;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Вводите символы, 'q' — для выхода.");
+
+        do
+        {
+            c = (char) br.read();
+            System.out.println(c);
+        } while(c != 'q') ;
     }
 }
